aec5eaa4fb36795cad4f39769cb10e86a5ea1093
Hussain-Mehdi <75681588+Hussain-Mehdi@users.noreply.github.com>
2023-05-23 07:39:55 +0500

    Functional Components in react

---
+++ src/components/Greet.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+//Functional Component
+
+// function Greet() {
+//     return (
+//         <h1>Hello Hussain</h1>
+//     );
+// }
+
+// export default Greet;
+
+
+
+// ES6 Callback component
+
+// const Greet = ()=> <h1>Hi Hussain</h1>
+// export default Greet;
+
+export const Greet = ()=> <h1>Assalam o Alikum Hussain</h1>
