3a0d7a497faba0bccb0d2d0a9842ecf7a4e1ce5a
Hussain-Mehdi <75681588+Hussain-Mehdi@users.noreply.github.com>
2023-05-28 23:46:55 +0500

    conditional variable rendering

--- src/components/Usergreeting.jsx
+++ src/components/Usergreeting.jsx
@@ -8,25 +8,40 @@ class Usergreeting extends Component {
       super(props)
     
       this.state = {
-         isLoggedIn:true
+         isLoggedIn:false
       }
     }
 
   render() {
-    if(this.state.isLoggedIn)
-    {
-        return(
-            <div>
-                <h2>User loggedIn</h2>
-            </div>
-        )
-    }else
-    return (
-      <div>
-        <h2>Not LoggedIn</h2>
-      </div>
-    )
-  }
+
+    // let message 
+
+    // if(this.state.isLoggedIn)
+    // {
+    //     message=<div>User LoggedIn</div>
+    // }
+    // else{
+    //     message=<div>User Not LoggedIn</div>
+    // }
+
+    // return <div>{message}</div>
+
+
+//     if(this.state.isLoggedIn)
+//     {
+//         return(
+//             <div>
+//                 <h2>User loggedIn</h2>
+//             </div>
+//         )
+//     }else
+//     return (
+//       <div>
+//         <h2>Not LoggedIn</h2>
+//       </div>
+//     )
+//   }
+}
 }
 
 export default Usergreeting
